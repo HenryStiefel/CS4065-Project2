f5e70edf9410402e1ce2bac4d2ce3a125def28d9
HenryStiefel <43274259+HenryStiefel@users.noreply.github.com>
2022-11-14 17:34:56 -0500

    Updating client and webserver

--- Client.py
+++ Client.py
@@ -3,41 +3,112 @@ import socket
 import sys
 from threading import *
  
+global server
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ip = "127.0.0.1"
 port = 9999
+global username
+username = ""
 
 def send_message():
     while True:
+        global stop_threads
+        if stop_threads:
+            break
+
         try:
             # wait for command line input
-            message = sys.stdin.readline()
+            message = sys.stdin.readline().strip('\n').split(' ')
 
-            if message:
+            # SEE COMMANDS
+            if message[0] == "COMMANDS":
+                print(
+                    "CONNECT [address] [port]\n" +
+                    "USERNAME\n" +
+                    "JOIN [room id]\n" + 
+                    "POST [subject] [content]\n" +
+                    "USERS\n" +
+                    "LEAVE\n" +
+                    "MESSAGE [message id]\n" + 
+                    "EXIT\n"
+                )
+
+            # CONNECT TO CHAT ROOM
+            elif message[0] == "CONNECT":
+                if len(message) == 3:
+                    try:
+                        connect(message[1], int(message[2]))
+                    except:
+                        print("Failed to connect to server.")
+                else:
+                    print("Invalid arguments.")
+
+            # POST NEW MESSAGE
+            elif message[0] == "POST":
+                if len(message) >= 3:
+                    server.send(' '.join(message).encode())
+                else:
+                    print("Invalid arguments.")
+
+            elif message[0] == "USERS":
+                print('.')
+            elif message[0] == "LEAVE":
+                print('.')
+
+            # REQUEST MESSAGE BY ID
+            elif message[0] == "MESSAGE":
+                if len(message) == 2:
+                    server.send(' '.join(message).encode())
+                else:
+                    print("Invalid arguments")
+
+            # LEAVE SERVER AND STOP CLIENT
+            elif message[0] == "EXIT":
+                stop_threads = True
+                exit()
+
+            # SET/INITIALIZE USERNAME
+            elif message[0] == "USERNAME":
                 # encode the message and send it to the server
-                server.send(message.encode())
-                print("<You> " + message)
+                if len(message) == 2:
+                    server.send(("USERNAME " + message[1]).encode())
+                else:
+                    print("Invalid arguments.")
+
+            else:
+                print("Invalid command, try again.")
         except:
             continue
 
 def receive_message():
     while True:
+        global stop_threads
+        if stop_threads:
+            break
         try:
             # receive bytes from the server
             # TODO: server needs to send the username as well
             message, addr = server.recvfrom(2048)
 
             # logic for client to get username here
-            if message.decode("UTF-8") == "Welcome to this chatroom!":
-                username = input("Please enter a username: ")
-                server.send(("CLIENT_USERNAME "+username).encode())
-                print("<You> " + username)
-            print("<> " + message)
+            if message:
+                if 'ping' not in message.decode("UTF-8"):
+                    print(message.decode("UTF-8"))
         except:
             continue
 
-# start both threads so we can listen for server messages and send our own commands 
-Thread(target=receive_message).start()
-Thread(target=send_message).start()
+def connect(ip, port):
+    server.connect((ip, port))
+def exit():
+    server.close()
+    print('Left server.')
 
-server.connect((ip, port))
+stop_threads = False
+
+# start both threads so we can listen for server messages and send our own commands 
+r = Thread(target=receive_message).start()
+s = Thread(target=send_message).start()
+
+print("To see a list of commands, please type \"COMMANDS\" (case sensitive)")
+
+#server.connect((ip, port))
--- WebServer.py
+++ WebServer.py
@@ -2,6 +2,8 @@ import socket
 import select
 import sys
 from _thread import *
+import datetime
+import time
  
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -13,28 +15,65 @@ server.bind((ip, port))
 server.listen(100)
  
 # TODO: make this a dictionary, map client-given username to connection
+global clients
+global messages
 clients = {}
+messages = []
  
 def clientthread(connection, address):
  
     # sends a message to the client connection
-    connection.send("Welcome to this chatroom!".encode())
-    print('sent welcome to client')
+    connection.send("Please enter a username with the \"USERNAME [your username here]\" command.".encode())
+
+    #TODO: send the previous 2 messages
+    #TODO: send a list of other usernames
  
     while True:
             try:
                 message = connection.recv(2048).decode("UTF-8")
                 
-                if message[:16] == "CLIENT_USERNAME ":
-                    clients[conn] = message[17:]
-                    broadcast("New user " + clients[conn] + " has entered the chat room!", connection)
-                if message:
-                    # print the message to the server terminal
-                    print ("<" + address[0] + "> " + message)
- 
-                    # TODO: we need the client username here
-                    message_to_send = "<" + address[0] + "> " + message
+                if clients[connection] == "NewUser":
+                    if message.split(' ')[0] == "USERNAME":
+                        message = message.split(' ')[1]
+                        if not clients.get(message):
+                            clients[connection] = message
+                            connection.send(("Username set successfully. Welcome, " + message).encode())
+                            broadcast("New user " + clients[connection] + " has entered the chat room!", connection)
+
+                            #lets send the previous 2 messages, other users, and rooms here
+                            if len(messages) > 0:
+                                print('.')
+                                #SEND THE PREVIOUS 1 OR 2 MESSAGES
+                            
+                            #send the clients list
+                            if len(clients) > 1:
+                                clientListMessage = "Other users:\n"
+                                for client in clients:
+                                    # don't send the current connection though
+                                    if client != connection:
+                                        clientListMessage += (clients[client] + '\n')
+                                connection.send(clientListMessage.encode())
+                        else:
+                            connection.send("That username is taken. Please try again.".encode())
+                    else:
+                        connection.send("Please enter a username first!".encode())
+                elif message.split(' ')[0] == "POST":
+                    newMessage = {
+                            "sender": clients[connection],
+                            "datetime": datetime.datetime.now(),
+                            "subject": message.split(' ')[1],
+                            "content": ' '.join(message.split(' ')[2:])
+                            }
+                    messages.append(newMessage)
+
+                    message_to_send = "Message ID: " + str(len(messages)) + " Sender: " + clients[connection] + " Time: " + newMessage["datetime"].strftime("%H:%M") + " Subject: " + newMessage["subject"]
+                    print(message_to_send)
                     broadcast(message_to_send, connection)
+                elif message.split(' ')[0] == "MESSAGE":
+                    if int(message.split(' ')[1]) < len(messages):
+                        connection.send(("Content: " + messages[int(message.split(' ')[1])]["content"]).encode())
+                    else:
+                        connection.send("No such message exists with that ID.".encode())
                 else:
                     # broken connection, remove the client
                     remove(connection)
@@ -43,10 +82,12 @@ def clientthread(connection, address):
  
 # broadcast to all connected clients except the one that sent the message
 def broadcast(message, connection):
+    print(message)
     for client in clients:
         if client != connection:
             try:
-                client.send(message)
+                client.send(message.encode())
+                print(message)
             except:
                 client.close()
  
@@ -55,10 +96,24 @@ def broadcast(message, connection):
  
 def remove(connection):
     if connection in clients:
-        clients.remove(connection)
+        print(clients[connection] + " left the chat.")
+        del clients[connection]
+
+def testconnections():
+    while True:
+        time.sleep(1)
+        try:
+            for client in clients:
+                try: 
+                    client.sendall(b"ping")
+                except:
+                    remove(client)
+        except:
+            pass      
+
+start_new_thread(testconnections, ())
  
 while True:
- 
     conn, addr = server.accept()
  
     clients[conn] = "NewUser"
@@ -68,7 +123,7 @@ while True:
     print (addr[0] + " connected")
  
     # creates thread for every user
-    start_new_thread(clientthread,(conn,addr))    
+    start_new_thread(clientthread,(conn,addr)) 
  
 conn.close()
 server.close()
